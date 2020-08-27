db96b2affe089962b79e5c0ecd11468f73644987
Rafda Hasna Imamah <3103119149@student.smktelkom-pwt.sch.id>
2020-08-27 10:13:21 -0700

    first committ

---
+++ src/main/kotlin/volume bola.kt
@@ -0,0 +1,15 @@
+package Challenge.VBola
+import java.util.*
+
+fun main() {
+    var jari: Float
+    var volumepi: Float
+    val reader = Scanner(System.`in`)
+    print("Masukkan jari jari : ")
+    jari = reader.nextFloat()
+    volumepi = ((jari * jari * jari * 22 * 4 / 7) / 3)
+    val s = String.format("%.2f", volumepi)
+    println("===========================")
+    print("Jadi Volume Bola = $s")
+}
+
